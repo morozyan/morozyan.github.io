0a6ceaa2bee4a7093a204a4e218d911bf4903dfe
morozyan <17960840+morozyan@users.noreply.github.com>
2023-10-26 22:54:26 -0600

    Update init.js

--- maps/guadalajara/init.js
+++ maps/guadalajara/init.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibW9yb3p5YW4iLCJhIjoiY2xtbG9pejdkMGN0dzJqbXd2b
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    style: 'mapbox://styles/morozyan/clo84qyo400o401qxgea14jz3', // style URL
     center: [-103.34872062829716, 20.672494028475253], // starting position [lng, lat]
     zoom: 11, // starting zoom
     maxBounds:[
