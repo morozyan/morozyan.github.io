ea4a5fda5c173c979064329070ebe86311a56fba
Roman Morozov <noreply>
2024-01-10 23:56:05 -0500

    updated Mexico map

--- maps/mexico/main.js
+++ maps/mexico/main.js
@@ -8,7 +8,7 @@ async function initMap() {
 
   map = new Map(document.querySelector("#MyMexico"), {
     center: { lat: 21.011806, lng: -101.368917 },
-    zoom: 4,
+    zoom: 5,
     mapId: "16284e329274360c", 
   });
 
